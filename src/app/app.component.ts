68b216a826dfe6a15c765e7d0b7c089c668b4224
Guilherme Corá <guilhermearcora@gmail.com>
2025-04-16 02:03:50 -0300

    Starting project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'front';
+
+  showAlert() {
+    Swal.fire("SweetAlert2 is working!");
+  }
 }
